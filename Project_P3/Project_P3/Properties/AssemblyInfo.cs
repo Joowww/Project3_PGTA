9bf6864507ce4ba18bdb3635a5293569dc7155a4
marinamartinf <marinamartinferrer@gmail.com>
2024-12-05 18:48:28 +0100

    trial2

--- Project_P3/Project_P3/Properties/AssemblyInfo.cs
+++ Project_P3/Project_P3/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Project_P3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("projecte PGTA")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PGTA")]
 [assembly: AssemblyProduct("Project_P3")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
